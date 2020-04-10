bba12f6ebf83ae2158d9c02fe66ee9e8df6556c9
Artyom <mr.bartem@mail.ru>
2020-04-10 18:44:09 +0700

    Initial Commit

---
+++ myFirstDatabase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myFirstDatabase
+//
+//  Created by Артём Бацанов on 10.04.2020.
+//  Copyright © 2020 Артём Бацанов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
