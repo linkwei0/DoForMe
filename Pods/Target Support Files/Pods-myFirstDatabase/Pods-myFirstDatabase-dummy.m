8562088e60b42b8d0e36471cd777251b2afd61a3
Artyom <mr.bartem@mail.ru>
2020-06-02 22:59:54 +0700

    Добавил Firebase в проект

---
+++ Pods/Target Support Files/Pods-myFirstDatabase/Pods-myFirstDatabase-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_myFirstDatabase : NSObject
+@end
+@implementation PodsDummy_Pods_myFirstDatabase
+@end
--- myFirstDatabase/ViewController.swift
+++ myFirstDatabase/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var btnViewSalons: UIBarButtonItem!
     override func viewDidLoad() {
         super.viewDidLoad()
+        //ha
 
     }
 
